5e9d2d3be7cb7d9678d47e95ba99f08b9ebb67b5
phu54321 <phu54321@naver.com>
2016-09-18 00:55:50 +0900

    이미지/css 등 리소스가 유실되어 이를 다시 복구.

---
+++ testdata/chosun_article_files/cs_art_ad_center_in.js
@@ -0,0 +1,13 @@
+// JavaScript Document for chosun.com article center up ad(x74)
+if(CatID){
+  Getcatid2 = CatID.substring(0,2);
+  if(Getcatid2 == "O2"){}
+  else {
+    if(/Android|iPad|iPhone|iPod/i.test(navigator.userAgent)){
+    }else {
+  if ( CatID.substring(0,2) !== "3N" ) {
+      document.write('<span style="width:250px; height:250px; line-height:0; float:right; margin-left:10px; margin-bottom;10px; margin-top:10px"><iframe width="250" height="250" noresize scrolling="no" frameborder="0" marginheight="0" marginwidth="0" src="http://cad.chosun.com/RealMedia/ads/adstream_sx.ads/www.chosun.com/news@x74"></iframe></span>');
+  }
+    }
+  }
+}
