4258935cc2facb39dd04592470dab1c6997e1df2
phu54321 <phu54321@naver.com>
2016-09-20 15:51:31 +0900

    dummyParser를 다른 파일로 이전
    dummyParser는 이후 다양하게 재사용이 가능하기 때문에 따로 분리

---
+++ test/dummyparser.py
@@ -0,0 +1,29 @@
+import re
+
+class DummyParser:
+    def parseNews(self, url):
+        newsID = int(re.match(r"dummy://news(\d+).html", url).group(1))
+        return {
+            'title': '뉴스 #%d' % newsID,
+            'author': '%d번 뉴스 작성자' % newsID,
+            'link': url,
+            'provider': 'dummy',
+            'category': '테스트 데이터 > 테스트',
+            'description': '',
+            'publishedAt': newsID,
+            'content': 'qwertyuiop',
+            'imageURL': ''
+        }
+
+
+    def parseNewsList(self, page):
+        newsList = []
+        for newsID in range(10 * (page - 1) + 1, 10 * page + 1):
+            newsURL = 'dummy://news%d.html' % newsID
+            newsList.append({
+                'title': '뉴스 #%d' % newsID,
+                'url': newsURL,
+                'providerNewsID': newsID
+            })
+
+        return newsList
--- test/test_crawler.py
+++ test/test_crawler.py
@@ -1,35 +1,7 @@
 import unittest
 from crawler import crawler
 import re
-
-class DummyParser:
-    def parseNews(self, url):
-        newsID = int(re.match(r"dummy://news(\d+).html", url).group(1))
-        return {
-            'title': '뉴스 #%d' % newsID,
-            'author': '%d번 뉴스 작성자' % newsID,
-            'link': url,
-            'provider': 'dummy',
-            'category': '테스트 데이터 > 테스트',
-            'description': '',
-            'publishedAt': newsID,
-            'content': 'qwertyuiop',
-            'imageURL': ''
-        }
-
-
-    def parseNewsList(self, page):
-        newsList = []
-        for newsID in range(10 * (page - 1) + 1, 10 * page + 1):
-            newsURL = 'dummy://news%d.html' % newsID
-            newsList.append({
-                'title': '뉴스 #%d' % newsID,
-                'url': newsURL,
-                'providerNewsID': newsID
-            })
-
-        return newsList
-
+from test.dummyparser import DummyParser
 
 class TestCrawler(unittest.TestCase):
     def test_dummyParser(self):
