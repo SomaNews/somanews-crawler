508633293771fa25b9902a068fffe533a8c2d3b3
phu54321 <phu54321@naver.com>
2016-10-01 10:14:23 +0900

    cron_crawler 추가
    크론 작업용으로 쓰기 좋은 cron_crawler를 추가했습니다.

---
+++ cron_crawler.py
@@ -0,0 +1,36 @@
+
+from crawler import (
+    dbconn,
+    crawler,
+
+    chosun,
+    donga,
+    hani,
+    khan
+)
+
+def main():
+    parserClassDict = {
+        'chosun': chosun.ParserChosun,
+        'donga': donga.ParserDonga,
+        'hani': hani.ParserHani,
+        'khan': khan.ParserKhan
+    }
+
+    db = dbconn.NewsDatabase('mongodb://localhost:27017/', 'somanews')
+
+    for provider, parserClass in parserClassDict.items():
+        print('Crawling from provider %s...' % provider)
+        latestNews = db.getLatestNews(provider)
+
+        if not latestNews:
+            # Cronable crawler only accept provider
+            continue
+
+        parser = parserClass()
+        crawlStartTime = latestNews['publishedAt']
+        crawler.crawlSince(db, parser, crawlStartTime)
+
+
+if __name__ == '__main__':
+    main()
